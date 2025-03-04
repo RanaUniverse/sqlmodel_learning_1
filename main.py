2a7b3bb50f607e6747e2d54ca2ff731bb0ddb4ff
RanaUniverse <RanaUniverse321@gmail.com>
2025-03-04 20:14:35 +0530

    taste a sqlmodel structer

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from sqlmodel-learning-1!")
+from typing import Optional
+
+from sqlmodel import (
+    Field,
+    SQLModel,
+)
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name:str
+    secret_name:str
+    age: Optional[int] = None
 
 
-if __name__ == "__main__":
-    main()
+hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+hero_2 = Hero(name="Spider-boy", secret_name="Pedro parqueador")
+hero_3 = Hero(name="Rusty Man", secret_name="Tommy sharp", age=48)
+
+
+print(hero_1)
+print(hero_2)
+print(hero_3)
+
+
